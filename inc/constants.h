59f45331a605b53d041bad62b5ebc5812bdb692d
Lorenzo Gualniera <lorenzo.gualniera@gmail.com>
2023-11-25 14:55:14 +0100

    move constants to dedicated header file
    Signed-off-by: Lorenzo Gualniera <lorenzo.gualniera@gmail.com>

---
+++ inc/constants.h
@@ -0,0 +1,249 @@
+/* Header Guard ------------------------------------------------------------- */
+
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+/* Includes ----------------------------------------------------------------- */
+
+#include <raylib.h>
+
+/* Definitions -------------------------------------------------------------- */
+
+#define GUI_WINDOW_TITLE  "Cippotto GUI"
+#define GUI_WINDOW_WIDTH  1520
+#define GUI_WINDOW_HEIGHT 552
+
+#define GUI_GRID_SPACING  24
+
+#define GUI_REGS_TITLE  "Registers"
+#define GUI_REGS_X      (GUI_GRID_SPACING)
+#define GUI_REGS_Y      (GUI_GRID_SPACING)
+#define GUI_REGS_WIDTH  264
+#define GUI_REGS_HEIGHT 504
+
+#define GUI_V0_X      (GUI_REGS_X + GUI_GRID_SPACING * (2))
+#define GUI_V0_Y      (GUI_REGS_Y + GUI_GRID_SPACING)
+#define GUI_V0_WIDTH  (GUI_GRID_SPACING * (3))
+#define GUI_V0_HEIGHT (GUI_GRID_SPACING)
+
+#define GUI_V1_X      (GUI_V0_X)
+#define GUI_V1_Y      (GUI_V0_Y + (32 * 1))
+#define GUI_V1_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V1_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V2_X      (GUI_V0_X)
+#define GUI_V2_Y      (GUI_V0_Y + (32 * 2))
+#define GUI_V2_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V2_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V3_X      (GUI_V0_X)
+#define GUI_V3_Y      (GUI_V0_Y + (32 * 3))
+#define GUI_V3_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V3_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V4_X      (GUI_V0_X)
+#define GUI_V4_Y      (GUI_V0_Y + (32 * 4))
+#define GUI_V4_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V4_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V5_X      (GUI_V0_X)
+#define GUI_V5_Y      (GUI_V0_Y + (32 * 5))
+#define GUI_V5_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V5_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V6_X      (GUI_V0_X)
+#define GUI_V6_Y      (GUI_V0_Y + (32 * 6))
+#define GUI_V6_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V6_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V7_X      (GUI_V0_X)
+#define GUI_V7_Y      (GUI_V0_Y + (32 * 7))
+#define GUI_V7_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V7_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V8_X      (GUI_REGS_X + GUI_GRID_SPACING * (7))
+#define GUI_V8_Y      (GUI_V0_Y)
+#define GUI_V8_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V8_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_V9_X      (GUI_V8_X)
+#define GUI_V9_Y      (GUI_V1_Y)
+#define GUI_V9_WIDTH  (GUI_V0_WIDTH)
+#define GUI_V9_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_VA_X      (GUI_V8_X)
+#define GUI_VA_Y      (GUI_V2_Y)
+#define GUI_VA_WIDTH  (GUI_V0_WIDTH)
+#define GUI_VA_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_VB_X      (GUI_V8_X)
+#define GUI_VB_Y      (GUI_V3_Y)
+#define GUI_VB_WIDTH  (GUI_V0_WIDTH)
+#define GUI_VB_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_VC_X      (GUI_V8_X)
+#define GUI_VC_Y      (GUI_V4_Y)
+#define GUI_VC_WIDTH  (GUI_V0_WIDTH)
+#define GUI_VC_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_VD_X      (GUI_V8_X)
+#define GUI_VD_Y      (GUI_V5_Y)
+#define GUI_VD_WIDTH  (GUI_V0_WIDTH)
+#define GUI_VD_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_VE_X      (GUI_V8_X)
+#define GUI_VE_Y      (GUI_V6_Y)
+#define GUI_VE_WIDTH  (GUI_V0_WIDTH)
+#define GUI_VE_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_VF_X      (GUI_V8_X)
+#define GUI_VF_Y      (GUI_V7_Y)
+#define GUI_VF_WIDTH  (GUI_V0_WIDTH)
+#define GUI_VF_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_TIM_X      (GUI_REGS_X + GUI_GRID_SPACING)
+#define GUI_TIM_Y      (GUI_V7_Y + (40))
+#define GUI_TIM_WIDTH  (GUI_REGS_WIDTH - GUI_GRID_SPACING * (2))
+#define GUI_TIM_HEIGHT 16
+
+#define GUI_DT_X      (GUI_V0_X)
+#define GUI_DT_Y      (GUI_TIM_Y + (32))
+#define GUI_DT_WIDTH  (GUI_V0_WIDTH)
+#define GUI_DT_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_ST_X      (GUI_V8_X)
+#define GUI_ST_Y      (GUI_DT_Y)
+#define GUI_ST_WIDTH  (GUI_V0_WIDTH)
+#define GUI_ST_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_SPE_X      (GUI_TIM_X)
+#define GUI_SPE_Y      (GUI_DT_Y + (40))
+#define GUI_SPE_WIDTH  (GUI_TIM_WIDTH)
+#define GUI_SPE_HEIGHT (GUI_TIM_HEIGHT)
+
+#define GUI_I_X      (GUI_V0_X)
+#define GUI_I_Y      (GUI_SPE_Y + (32 * 1))
+#define GUI_I_WIDTH  (GUI_V0_WIDTH)
+#define GUI_I_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_PC_X      (GUI_V0_X)
+#define GUI_PC_Y      (GUI_SPE_Y + (32 * 2))
+#define GUI_PC_WIDTH  (GUI_V0_WIDTH)
+#define GUI_PC_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_SP_X      (GUI_V0_X)
+#define GUI_SP_Y      (GUI_SPE_Y + (32 * 3))
+#define GUI_SP_WIDTH  (GUI_V0_WIDTH)
+#define GUI_SP_HEIGHT (GUI_V0_HEIGHT)
+
+#define GUI_GAME_TITLE  "Game"
+#define GUI_GAME_X      (GUI_REGS_X + GUI_REGS_WIDTH + GUI_GRID_SPACING)
+#define GUI_GAME_Y      (GUI_REGS_Y)
+#define GUI_GAME_WIDTH  896
+#define GUI_GAME_HEIGHT (GUI_REGS_HEIGHT)
+
+#define GUI_DRAW_X      (GUI_GAME_X + GUI_GRID_SPACING)
+#define GUI_DRAW_Y      (GUI_GAME_Y + GUI_GRID_SPACING)
+#define GUI_DRAW_WIDTH  (GUI_GAME_WIDTH - GUI_GRID_SPACING * (2))
+#define GUI_DRAW_HEIGHT (GUI_GAME_HEIGHT - GUI_GRID_SPACING * (2))
+
+#define GUI_ASM_TITLE  "Assembly"
+#define GUI_ASM_X      (GUI_GAME_X + GUI_GAME_WIDTH + GUI_GRID_SPACING)
+#define GUI_ASM_Y      (GUI_REGS_Y)
+#define GUI_ASM_WIDTH  (GUI_REGS_WIDTH)
+#define GUI_ASM_HEIGHT (GUI_REGS_HEIGHT)
+
+#define GUI_OPS_BUFFER_LEN 100
+#define GUI_OPS_X          0
+#define GUI_OPS_Y          0
+#define GUI_OPS_WIDTH      (GUI_REGS_WIDTH - 14)
+#define GUI_OPS_HEIGHT     (GUI_GRID_SPACING * (GUI_OPS_BUFFER_LEN + 2))
+
+/* Data types --------------------------------------------------------------- */
+
+typedef enum {
+    LAYOUT_REGS, // GroupBox:    Registers
+    LAYOUT_V0,   // ValueBox:    V0
+    LAYOUT_V1,   // ValueBox:    V1
+    LAYOUT_V2,   // ValueBox:    V2
+    LAYOUT_V3,   // ValueBox:    V3
+    LAYOUT_V4,   // ValueBox:    V4
+    LAYOUT_V5,   // ValueBox:    V5
+    LAYOUT_V6,   // ValueBox:    V6
+    LAYOUT_V7,   // ValueBox:    V7
+    LAYOUT_V8,   // ValueBox:    V8
+    LAYOUT_V9,   // ValueBox:    V9
+    LAYOUT_VA,   // ValueBox:    VA
+    LAYOUT_VB,   // ValueBox:    VB
+    LAYOUT_VC,   // ValueBox:    VC
+    LAYOUT_VD,   // ValueBox:    VD
+    LAYOUT_VE,   // ValueBox:    VE
+    LAYOUT_VF,   // ValueBox:    VF
+    LAYOUT_TIM,  // Line:        Timers
+    LAYOUT_DT,   // ValueBox:    DT
+    LAYOUT_ST,   // ValueBox:    ST
+    LAYOUT_SPE,  // Line:        Special
+    LAYOUT_I,    // ValueBox:    I
+    LAYOUT_PC,   // ValueBox:    PC
+    LAYOUT_SP,   // ValueBox:    SP
+    LAYOUT_GAME, // GroupBox:    Game
+    LAYOUT_DRAW, // Panel:       Draw
+    LAYOUT_ASM,  // GroupBox:    Assembly
+    LAYOUT_OPS,  // ScrollPanel: Ops
+} layout_id_t;
+
+/* Global variables --------------------------------------------------------- */
+
+static const Rectangle layout[] = {
+    (Rectangle){ GUI_REGS_X, GUI_REGS_Y, GUI_REGS_WIDTH, GUI_REGS_HEIGHT },
+    (Rectangle){ GUI_V0_X,   GUI_V0_Y,   GUI_V0_WIDTH,   GUI_V0_HEIGHT   },
+    (Rectangle){ GUI_V1_X,   GUI_V1_Y,   GUI_V1_WIDTH,   GUI_V1_HEIGHT   },
+    (Rectangle){ GUI_V2_X,   GUI_V2_Y,   GUI_V2_WIDTH,   GUI_V2_HEIGHT   },
+    (Rectangle){ GUI_V3_X,   GUI_V3_Y,   GUI_V3_WIDTH,   GUI_V3_HEIGHT   },
+    (Rectangle){ GUI_V4_X,   GUI_V4_Y,   GUI_V4_WIDTH,   GUI_V4_HEIGHT   },
+    (Rectangle){ GUI_V5_X,   GUI_V5_Y,   GUI_V5_WIDTH,   GUI_V5_HEIGHT   },
+    (Rectangle){ GUI_V6_X,   GUI_V6_Y,   GUI_V6_WIDTH,   GUI_V6_HEIGHT   },
+    (Rectangle){ GUI_V7_X,   GUI_V7_Y,   GUI_V7_WIDTH,   GUI_V7_HEIGHT   },
+    (Rectangle){ GUI_V8_X,   GUI_V8_Y,   GUI_V8_WIDTH,   GUI_V8_HEIGHT   },
+    (Rectangle){ GUI_V9_X,   GUI_V9_Y,   GUI_V9_WIDTH,   GUI_V9_HEIGHT   },
+    (Rectangle){ GUI_VA_X,   GUI_VA_Y,   GUI_VA_WIDTH,   GUI_VA_HEIGHT   },
+    (Rectangle){ GUI_VB_X,   GUI_VB_Y,   GUI_VB_WIDTH,   GUI_VB_HEIGHT   },
+    (Rectangle){ GUI_VC_X,   GUI_VC_Y,   GUI_VC_WIDTH,   GUI_VC_HEIGHT   },
+    (Rectangle){ GUI_VD_X,   GUI_VD_Y,   GUI_VD_WIDTH,   GUI_VD_HEIGHT   },
+    (Rectangle){ GUI_VE_X,   GUI_VE_Y,   GUI_VE_WIDTH,   GUI_VE_HEIGHT   },
+    (Rectangle){ GUI_VF_X,   GUI_VF_Y,   GUI_VF_WIDTH,   GUI_VF_HEIGHT   },
+    (Rectangle){ GUI_TIM_X,  GUI_TIM_Y,  GUI_TIM_WIDTH,  GUI_TIM_HEIGHT  },
+    (Rectangle){ GUI_DT_X,   GUI_DT_Y,   GUI_DT_WIDTH,   GUI_DT_HEIGHT   },
+    (Rectangle){ GUI_ST_X,   GUI_ST_Y,   GUI_ST_WIDTH,   GUI_ST_HEIGHT   },
+    (Rectangle){ GUI_SPE_X,  GUI_SPE_Y,  GUI_SPE_WIDTH,  GUI_SPE_HEIGHT  },
+    (Rectangle){ GUI_I_X,    GUI_I_Y,    GUI_I_WIDTH,    GUI_I_HEIGHT    },
+    (Rectangle){ GUI_PC_X,   GUI_PC_Y,   GUI_PC_WIDTH,   GUI_PC_HEIGHT   },
+    (Rectangle){ GUI_SP_X,   GUI_SP_Y,   GUI_SP_WIDTH,   GUI_SP_HEIGHT   },
+    (Rectangle){ GUI_GAME_X, GUI_GAME_Y, GUI_GAME_WIDTH, GUI_GAME_HEIGHT },
+    (Rectangle){ GUI_DRAW_X, GUI_DRAW_Y, GUI_DRAW_WIDTH, GUI_DRAW_HEIGHT },
+    (Rectangle){ GUI_ASM_X,  GUI_ASM_Y,  GUI_ASM_WIDTH,  GUI_ASM_HEIGHT  },
+    (Rectangle){ GUI_OPS_X,  GUI_OPS_Y,  GUI_OPS_WIDTH,  GUI_OPS_HEIGHT  },
+};
+
+static const int keys[] = {
+    KEY_ZERO,
+    KEY_ONE,
+    KEY_TWO,
+    KEY_THREE,
+    KEY_FOUR,
+    KEY_FIVE,
+    KEY_SIX,
+    KEY_SEVEN,
+    KEY_EIGHT,
+    KEY_NINE,
+    KEY_A,
+    KEY_B,
+    KEY_C,
+    KEY_D,
+    KEY_E,
+    KEY_F,
+};
+
+#endif /* __CONSTANTS_H__ */
+
+/* -------------------------------------------------------------------------- */
--- src/main.c
+++ src/main.c
@@ -9,85 +9,21 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 #include "style_cyber.h"
+
+#include "constants.h"
 #include "chip8.h"
-
-/* Definitions -------------------------------------------------------------- */
-
-#define WINDOW_WIDTH  1520
-#define WINDOW_HEIGHT 552
-
-#define LAYOUT_GROUP_REGISTERS 0
-#define LAYOUT_VALUE_V0        1
-#define LAYOUT_LINE_TIMERS     17
-#define LAYOUT_VALUE_DT        18
-#define LAYOUT_VALUE_ST        19
-#define LAYOUT_LINE_SPECIAL    20
-#define LAYOUT_VALUE_I         21
-#define LAYOUT_VALUE_PC        22
-#define LAYOUT_VALUE_SP        23
-#define LAYOUT_GROUP_GAME      24
-#define LAYOUT_GROUP_ASM       25
 
 /* Function Prototypes ------------------------------------------------------ */
 
-static void UpdateRegValue(char *text, void *reg, int size, int *value, bool *edit, Rectangle rec);
+static void InitCippottoGui(void);
+static void ReadInputKeys(chip8_t *ch8);
 static void UpdateRegsView(chip8_t *ch8);
+static void UpdateRegValue(char *text, void *reg, int size, int *value,
+                           bool *edit, Rectangle rec);
 static void UpdateGameView(chip8_t *ch8);
 static void UpdateASMView(chip8_t *ch8);
-static void ReadInputKeys(chip8_t *ch8);
 
 /* Global variables --------------------------------------------------------- */
-
-static const int raylib_keys[] = {
-    KEY_ZERO,
-    KEY_ONE,
-    KEY_TWO,
-    KEY_THREE,
-    KEY_FOUR,
-    KEY_FIVE,
-    KEY_SIX,
-    KEY_SEVEN,
-    KEY_EIGHT,
-    KEY_NINE,
-    KEY_A,
-    KEY_B,
-    KEY_C,
-    KEY_D,
-    KEY_E,
-    KEY_F,
-};
-
-static const Rectangle layout[] = {
-    (Rectangle){   24,  24, 264, 504 }, // GroupBox: Registers
-    (Rectangle){   72,  48,  72,  24 }, // ValueBox: V0
-    (Rectangle){   72,  80,  72,  24 }, // ValueBox: V1
-    (Rectangle){   72, 112,  72,  24 }, // ValueBox: V2
-    (Rectangle){   72, 144,  72,  24 }, // ValueBox: V3
-    (Rectangle){   72, 176,  72,  24 }, // ValueBox: V4
-    (Rectangle){   72, 208,  72,  24 }, // ValueBox: V5
-    (Rectangle){   72, 240,  72,  24 }, // ValueBox: V6
-    (Rectangle){   72, 272,  72,  24 }, // ValueBox: V7
-    (Rectangle){  192,  48,  72,  24 }, // ValueBox: V8
-    (Rectangle){  192,  80,  72,  24 }, // ValueBox: V9
-    (Rectangle){  192, 112,  72,  24 }, // ValueBox: VA
-    (Rectangle){  192, 144,  72,  24 }, // ValueBox: VB
-    (Rectangle){  192, 176,  72,  24 }, // ValueBox: VC
-    (Rectangle){  192, 208,  72,  24 }, // ValueBox: VD
-    (Rectangle){  192, 240,  72,  24 }, // ValueBox: VE
-    (Rectangle){  192, 272,  72,  24 }, // ValueBox: VF
-    (Rectangle){   48, 312, 216,  16 }, // Line:     Timers
-    (Rectangle){   72, 344,  72,  24 }, // ValueBox: DT
-    (Rectangle){  192, 344,  72,  24 }, // ValueBox: ST
-    (Rectangle){   48, 384, 216,  16 }, // Line:     Special
-    (Rectangle){   72, 416,  72,  24 }, // ValueBox: I
-    (Rectangle){   72, 448,  72,  24 }, // ValueBox: PC
-    (Rectangle){   72, 480,  72,  24 }, // ValueBox: SP
-    (Rectangle){  312,  24, 896, 504 }, // GroupBox: Game
-    (Rectangle){ 1232,  24, 264, 504 }, // GroupBox: Assembly
-};
-
-static Color background_color = BLACK;
-static Color foreground_color = WHITE;
 
 // ValueBox: V0-VF
 static int Vi_value[CHIP8_REGS_NUM] = {0};
@@ -113,9 +49,23 @@ static bool PC_edit = false;
 static int SP_value = 0;
 static bool SP_edit = false;
 
+// ScrollPanel: Assembly
+static char ops_buffer[GUI_OPS_BUFFER_LEN][CHIP8_DECODE_STR_SIZE] = {0};
+static int ops_counter = 0;
+static Rectangle ops_view = {0};
+static Vector2 ops_scroll = {0, 0};
+
+// Style colors
+static Color background_color = {0};
+static Color line_color = {0};
+static Color border_color[4] = {0};
+static Color base_color[4] = {0};
+static Color text_color[4] = {0};
+
 /* Function definitions ----------------------------------------------------- */
 
-void UpdateRegValue(char *text, void *reg, int size, int *value, bool *edit, Rectangle rec)
+void UpdateRegValue(char *text, void *reg, int size, int *value, bool *edit,
+                    Rectangle rec)
 {
     if (!(*edit))
     {
@@ -132,58 +82,122 @@ void UpdateRegValue(char *text, void *reg, int size, int *value, bool *edit, Rec
 
 void UpdateRegsView(chip8_t *ch8)
 {
-    GuiGroupBox(layout[LAYOUT_GROUP_REGISTERS], "Registers");
+    GuiGroupBox(layout[LAYOUT_REGS], "Registers");
     for (uint8_t i = 0; i < CHIP8_REGS_NUM; ++i)
     {
         char Vi[5];
         sprintf(Vi, "V%X ", i);
-        UpdateRegValue(Vi, &ch8->V[i], sizeof(uint8_t), &Vi_value[i], &Vi_edit[i], layout[LAYOUT_VALUE_V0 + i]);
+        UpdateRegValue(Vi, &ch8->V[i], sizeof(uint8_t), &Vi_value[i],
+                       &Vi_edit[i], layout[LAYOUT_V0 + i]);
     }
-    GuiLine(layout[LAYOUT_LINE_TIMERS], NULL);
-    UpdateRegValue("DT ", &ch8->DT, sizeof(uint8_t), &DT_value, &DT_edit, layout[LAYOUT_VALUE_DT]);
-    UpdateRegValue("ST ", &ch8->ST, sizeof(uint8_t), &ST_value, &ST_edit, layout[LAYOUT_VALUE_ST]);
-    GuiLine(layout[LAYOUT_LINE_SPECIAL], NULL);
-    UpdateRegValue("I ", &ch8->I, sizeof(uint16_t), &I_value, &I_edit, layout[LAYOUT_VALUE_I]);
-    UpdateRegValue("PC ", &ch8->PC, sizeof(uint16_t), &PC_value, &PC_edit, layout[LAYOUT_VALUE_PC]);
-    UpdateRegValue("SP ", &ch8->SP, sizeof(uint16_t), &SP_value, &SP_edit, layout[LAYOUT_VALUE_SP]);
+    GuiLine(layout[LAYOUT_TIM], NULL);
+    UpdateRegValue("DT ", &ch8->DT, sizeof(uint8_t), &DT_value, &DT_edit,
+                   layout[LAYOUT_DT]);
+    UpdateRegValue("ST ", &ch8->ST, sizeof(uint8_t), &ST_value, &ST_edit,
+                   layout[LAYOUT_ST]);
+    GuiLine(layout[LAYOUT_SPE], NULL);
+    UpdateRegValue("I ", &ch8->I, sizeof(uint16_t), &I_value, &I_edit,
+                   layout[LAYOUT_I]);
+    UpdateRegValue("PC ", &ch8->PC, sizeof(uint16_t), &PC_value, &PC_edit,
+                   layout[LAYOUT_PC]);
+    UpdateRegValue("SP ", &ch8->SP, sizeof(uint16_t), &SP_value, &SP_edit,
+                   layout[LAYOUT_SP]);
 }
 
 void UpdateGameView(chip8_t *ch8)
 {
-    Rectangle rect = layout[LAYOUT_GROUP_GAME];
-    float scale_x = rect.width / CHIP8_DISPLAY_WIDTH;
-    float scale_y = rect.height / CHIP8_DISPLAY_HEIGHT;
+    GuiGroupBox(layout[LAYOUT_GAME], GUI_GAME_TITLE);
+
+    Rectangle panel = layout[LAYOUT_DRAW];
+    float scale_x = panel.width / CHIP8_DISPLAY_WIDTH;
+    float scale_y = panel.height / CHIP8_DISPLAY_HEIGHT;
 
     for (uint8_t x = 0; x < CHIP8_DISPLAY_WIDTH; ++x)
     {
         for (uint8_t y = 0; y < CHIP8_DISPLAY_HEIGHT; ++y)
         {
             Rectangle pixel = {
-                rect.x + (x * scale_x),
-                rect.y + (y * scale_y),
+                panel.x + (x * scale_x),
+                panel.y + (y * scale_y),
                 scale_x,
                 scale_y,
             };
 
             if (ch8->display[x + y * CHIP8_DISPLAY_WIDTH])
-                DrawRectangleRec(pixel, foreground_color);
+                DrawRectangleRec(pixel, line_color);
         }
     }
-
-    GuiGroupBox(rect, "Game");
 }
 
 void UpdateASMView(chip8_t *ch8)
 {
-    GuiGroupBox(layout[LAYOUT_GROUP_ASM], "Assembly");
+    Rectangle panel = layout[LAYOUT_ASM];
+    Rectangle content = layout[LAYOUT_OPS];
+
+    float pos_x = panel.x + ops_scroll.x + GUI_GRID_SPACING;
+    float pos_y = panel.y + ops_scroll.y + GUI_GRID_SPACING;
+
+    GuiScrollPanel(panel, NULL, content, &ops_scroll, &ops_view);
+    DrawRectangleRec((Rectangle){panel.x + 8, panel.y, 67, 1}, background_color);
+    GuiGroupBox(panel, GUI_ASM_TITLE);
+
+    BeginScissorMode(ops_view.x, ops_view.y, ops_view.width, ops_view.height);
+
+    for (int i = 0; i < 25; ++i)
+    {
+        uint16_t PC = ch8->PC + (i * sizeof(uint16_t));
+        if (PC > CHIP8_RAM_END) break;
+
+        uint16_t raw = ((uint16_t)ch8->ram[PC] << 8) | ch8->ram[PC + 1];
+        chip8_op_t op;
+        chip8_cmd_t cmd;
+        chip8_err_t err;
+        static char addr_str[5];
+
+        sprintf(addr_str, "0x%04X", PC);
+        chip8_parse(&op, raw);
+        err = chip8_decode(&cmd, ops_buffer[i], op);
+        if (err) sprintf(ops_buffer[i], "UNKNOWN");
+
+        float text_y = pos_y + (i * 24);
+        Color color = (i == 0) ? text_color[2] : text_color[0];
+        DrawText(addr_str, pos_x, text_y, 18, color);
+        DrawText(ops_buffer[i], pos_x + 84, text_y, 18, color);
+    }
+
+    EndScissorMode();
 }
 
 void ReadInputKeys(chip8_t *ch8)
 {
     memset(ch8->keys, 0x0, sizeof(uint8_t) * CHIP8_KEYS_SIZE);
-
     for (uint8_t i = 0; i < CHIP8_KEYS_SIZE; ++i)
-        ch8->keys[i] = IsKeyDown(raylib_keys[i]);
+        ch8->keys[i] = IsKeyDown(keys[i]);
+}
+
+void InitCippottoGui(void)
+{
+    InitWindow(GUI_WINDOW_WIDTH, GUI_WINDOW_HEIGHT, GUI_WINDOW_TITLE);
+    SetTargetFPS(CHIP8_CLOCK_HZ);
+    GuiLoadStyleCyber();
+
+    background_color = GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR));
+    line_color = GetColor(GuiGetStyle(DEFAULT, LINE_COLOR));
+
+    border_color[0] = GetColor(GuiGetStyle(DEFAULT, BORDER_COLOR_NORMAL));
+    border_color[1] = GetColor(GuiGetStyle(DEFAULT, BORDER_COLOR_FOCUSED));
+    border_color[2] = GetColor(GuiGetStyle(DEFAULT, BORDER_COLOR_PRESSED));
+    border_color[3] = GetColor(GuiGetStyle(DEFAULT, BORDER_COLOR_DISABLED));
+
+    base_color[0] = GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_NORMAL));
+    base_color[1] = GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_FOCUSED));
+    base_color[2] = GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_PRESSED));
+    base_color[3] = GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_DISABLED));
+
+    text_color[0] = GetColor(GuiGetStyle(DEFAULT, TEXT_COLOR_NORMAL));
+    text_color[1] = GetColor(GuiGetStyle(DEFAULT, TEXT_COLOR_FOCUSED));
+    text_color[2] = GetColor(GuiGetStyle(DEFAULT, TEXT_COLOR_PRESSED));
+    text_color[3] = GetColor(GuiGetStyle(DEFAULT, TEXT_COLOR_DISABLED));
 }
 
 int main(int argc, char **argv)
@@ -209,13 +223,7 @@ int main(int argc, char **argv)
     chip8_init(&chip8, chip8_cfg);
     fclose(rom);
 
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Cippotto GUI");
-    SetTargetFPS(CHIP8_CLOCK_HZ);
-
-    GuiLoadStyleCyber();
-    background_color = GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR));
-    foreground_color = GetColor(GuiGetStyle(DEFAULT, LINE_COLOR));
-
+    InitCippottoGui();
     while (!WindowShouldClose() && !chip8_run(&chip8))
     {
         BeginDrawing();
